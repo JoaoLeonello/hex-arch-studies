66cd434cdb544112585dd43b253d4dcd65e8d367
JoaoLeonello <jfelipe.pl@gmail.com>
2023-05-18 11:00:43 -0300

    Adds cli interface

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/JoaoLeonello/hex-arch-studies/adapters/db"
-	"github.com/JoaoLeonello/hex-arch-studies/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/JoaoLeonello/hex-arch-studies/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Exemple", 30)
-
-	productService.Enable(product)
+	cmd.Execute()
 }
